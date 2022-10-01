fe660b28cd90d8e8ae4eff61dff0dfb818ddcb1c
Nicholas Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2022-10-01 05:31:51 -0700

    Added author field to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=80)
     body = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
@@ -19,6 +21,10 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
 
     class Meta:
         ordering = ['-datetime_created']
