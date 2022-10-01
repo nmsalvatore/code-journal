d092763764e63dc723d40672a87ffddad74dd74e
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-10-01 06:05:17 -0700

    Added tags field to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.urls import reverse
 from django.db import models
 from datetime import timedelta
@@ -9,6 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=80)
     body = models.TextField()
+    tags = ArrayField(models.CharField(max_length=10, blank=True), null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     date_updated = models.DateTimeField(auto_now=True, null=True)
     slug = models.SlugField(null=True, unique=True)
