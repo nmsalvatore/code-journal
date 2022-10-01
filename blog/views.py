57c1362055b7ec5386d26d32d4dd278173431cd4
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-10-01 05:42:48 -0700

    Removed HttpResponse import

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from http.client import HTTPResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
